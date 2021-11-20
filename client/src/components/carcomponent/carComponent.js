1f6b19d794dccebe1ad608f1dffba3a77db645b6
NigamBhattarai <inigambhattarai@gmail.com>
2021-11-20 20:37:37 +0545

    added: image gallery to car component

--- client/src/components/carcomponent/carComponent.js
+++ client/src/components/carcomponent/carComponent.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useCallback, useEffect, useState } from "react";
 import { Row, Col, Container, Button } from "react-bootstrap";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
@@ -9,6 +9,9 @@ import "./carcomponent.scss";
 import { useAlert } from "react-alert";
 import axios from "axios";
 import Loader from "../common/loader";
+import ImageGallery from "react-image-gallery";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes as close } from "@fortawesome/free-solid-svg-icons";
 
 const CarContext = createContext();
 
@@ -22,9 +25,17 @@ function CarComponent() {
   const [bookedId, setBookedId] = useState("");
   const [copied, setCopied] = useState(false);
   const [car, setCar] = useState();
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
   var [initCar] = useState();
+  const [galleryIsOpen, setGalleryIsOpen] = useState(false);
+  var images = [];
 
   //FUNCTIONS
+  document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 27) {
+      setGalleryIsOpen(false);
+    }
+  });
 
   const copyToClipboard = (text) => {
     if (booked) {
@@ -51,6 +62,39 @@ function CarComponent() {
       );
     }
   };
+
+  const isCarDefined = useCallback(() => {
+    return car !== undefined;
+  }, [car]);
+
+  const selectImage = (index) => {
+    setSelectedImageIndex(index);
+  };
+
+  const showAllImages = () => {
+    var items = [];
+    car.images.forEach((value, index, array) => {
+      items.push(
+        <Col
+          key={index}
+          xs={3}
+          xxl={3}
+          className="otherImagesItem"
+          onMouseOver={(e) => selectImage(index)}
+          onClick={(e)=>setGalleryIsOpen(true)}
+        >
+          <img
+            src={"http://localhost:3000" + value}
+            className="img-responsive img-fluid"
+            alt={"Image-" + index}
+          />
+        </Col>
+      );
+    });
+    return items;
+  };
+
+  //Callbacks
   useEffect(() => {
     if (booked)
       alert.show('Your car is booked with id: "' + bookedId + '" Copy this id');
@@ -65,60 +109,93 @@ function CarComponent() {
       .get("http://localhost:3000/car?id=" + id)
       .then((res) => {
         setCar(res.data);
-        // console.log(res.data);
       })
       .catch((error) => {
         console.log("Failure");
       });
   }, [initCar, id]);
-  // console.log(car)
-  const isCarDefined = () => {
-    return car !== undefined;
-  };
 
-  UseTitle(isCarDefined() ? car.name + " | Cars Cobra":"Cars Cobra");
+  useEffect(() => {
+    if (isCarDefined()) {
+      car.images.forEach((value, index, array) => {
+        images.push({
+          original: "http://localhost:3000" + value,
+          thumbnail: "http://localhost:3000" + value,
+        });
+      });
+    }
+  });
+
+  UseTitle(isCarDefined() ? car.name + " | Cars Cobra" : "Cars Cobra");
 
   return isCarDefined() ? (
-    <CarContext.Provider value={{}}>
-      <Header />
-      <Container className="carDetailsContainer">
-        <Row>
-          <Col md="5" className="my-auto">
-            <img
-              src={"http://localhost:3000" + car.images[0]}
-              alt="Car"
-              className="carImage img-responsive img-fluid"
-            />
-          </Col>
-          <Col md="7" className="carInfoDiv">
-            <Link to="/" className="carName">
-              {car.name}
-            </Link>
-            <div className="carDesc">
-              <Row className="mb-5">{car.desc}</Row>
-              <Button
-                type="submit"
-                className="bookNowButton border-0"
-                onClick={(e) => bookCar()}
-              >
-                Book Now
-              </Button>
-
-              {booked && (
-                <div
-                  className="text-success font-small mt-4"
-                  style={{ cursor: "pointer" }}
-                  onClick={(e) => copyToClipboard(bookedId)}
-                >
-                  ID: {bookedId} (Click to Copy)
-                </div>
-              )}
-            </div>
+    galleryIsOpen ? (
+      <Container fluid>
+        <Row className="closeBtnRow">
+          <Col
+            xs={1}
+            xxl={1}
+            icon={close}
+            className="closeBtn"
+            onClick={(e) => {
+              setGalleryIsOpen(false);
+            }}
+          >
+            <FontAwesomeIcon icon={close}/>
           </Col>
         </Row>
+        <ImageGallery items={images} startIndex={selectedImageIndex} />
       </Container>
-      <Footer />
-    </CarContext.Provider>
+    ) : (
+      <CarContext.Provider value={{}}>
+        <Header />
+        <Container className="carDetailsContainer">
+          <Row>
+            <Col md="5" className="my-auto">
+              <Row
+                style={{ cursor: "pointer" }}
+                onClick={(e) => {
+                  setGalleryIsOpen(true);
+                }}
+              >
+                <img
+                  src={"http://localhost:3000" + car.images[selectedImageIndex]}
+                  alt="Car"
+                  className="carImage img-responsive img-fluid"
+                />
+              </Row>
+              <Row className="my-3">{showAllImages()}</Row>
+            </Col>
+            <Col md="7" className="carInfoDiv">
+              <Link to="/" className="carName">
+                {car.name}
+              </Link>
+              <div className="carDesc">
+                <Row className="mb-5">{car.desc}</Row>
+                <Button
+                  type="submit"
+                  className="bookNowButton border-0"
+                  onClick={(e) => bookCar()}
+                >
+                  Book Now
+                </Button>
+
+                {booked && (
+                  <div
+                    className="text-success font-small mt-4"
+                    style={{ cursor: "pointer" }}
+                    onClick={(e) => copyToClipboard(bookedId)}
+                  >
+                    ID: {bookedId} (Click to Copy)
+                  </div>
+                )}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        <Footer />
+      </CarContext.Provider>
+    )
   ) : (
     <Loader active={true} />
   );
